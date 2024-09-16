10c4c4cc61955b63aaad8c93ff24013b62736706
rajdeepadak <rajdeepadak@gmail.com>
2024-09-16 04:38:16 -0700

    lab 2 updated

---
+++ Lab2/lab2_programs/cobot_fk.m
@@ -0,0 +1,63 @@
+% MATLAB code: R _|_ R || R || R _|_ R _|_ R _|_ R; 6 DOF cobot
+
+% clear terminal output
+clear all clc
+
+% Create all necessary symbols
+syms a1 a2 a3 a4 a5 t1 t2 t3 t4 t5 t6 the1 the2 the3 the4 the5 the6
+
+% Find all constants
+a1 = ; 
+a2 = ; 
+a3 = ; 
+a4 = ; 
+a5 = ; 
+a6 = ;
+
+% Find homogenous transformation matrices
+H01 = []
+H12 = []
+H23 = []
+H34 = []
+H45 = []
+H56 = []
+
+% Find H06
+H06 = 
+
+% End-effector position
+Px = 
+Py = 
+Pz = 
+
+% Test for any values of theta
+tt1 = 0/180*pi; % Theta1
+tt2 = 0/180*pi; % Theta2
+tt3 = 0/180*pi; % Theta3
+tt4 = 0/180*pi; % Theta4
+tt5 = 0/180*pi; % Theta5
+tt6 = 0/180*pi; % Theta6
+
+% Substitute the numerical theta values in place of the theta symbols
+px_new=
+py_new=
+pz_new=
+
+% Extract rotation matrix
+R06
+
+% Substitute numerical joint angles in place of the theta symbols
+R06_new 
+
+
+
+
+
+
+
+
+
+
+
+
+
